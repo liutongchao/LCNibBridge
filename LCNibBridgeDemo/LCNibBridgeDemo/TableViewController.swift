d352a7abc8cf3043041c1d9f7d3a8aa7aae87ddf
liutongchao <413281269@qq.com>
2016-06-12 12:41:35 +0800

    Delete method replace , use override method. And resolve the problem of crash when pop controller

---
+++ LCNibBridgeDemo/LCNibBridgeDemo/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  LCNibBridgeDemo
+//
+//  Created by 刘通超 on 16/6/12.
+//  Copyright © 2016年 刘通超. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "LCNibBridge"
+    }
+}
